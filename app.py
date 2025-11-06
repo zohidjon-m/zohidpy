ad8a9d0e9ef3bdbacdb2c445326aabdf05e8d829
zohidjon-m <148512253+zohidjon-m@users.noreply.github.com>
2025-11-06 09:56:26 +0900

    changes

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ class ZohidPyApp:
     def __call__(self, environ, start_response):
         request = Request(environ)     
         response = self.handle_request(request)
-        # ensure `response` is always a WebOb Response
-        # if not isinstance(response, Response):
-        #         response = Response(status=500, text="Internal Server Error")
-
         return response(environ, start_response)
   
   
@@ -27,19 +23,7 @@ class ZohidPyApp:
         self.default_response(response)
         return response
          
-    # def handle_request(self, request):
-    #     # Match path to registered handler
-    #     handler = self.routes.get(request.path)
 
-    #     # If no handler, return 404 Response
-    #     if handler is None:
-    #         return Response(status=404, text=f"404 Not Found: {request.path}")
-
-    #     # Build a Response object and let handler fill it
-    #     response = Response()
-    #     handler(request, response)
-    #     return response
-    
     def default_response(self, response):
         response.status_code = 404
         response.text = "Not Found."
